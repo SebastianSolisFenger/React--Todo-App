db322bf01eff30ce9f9308d4a7036dbf08d33ed1
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-26 19:53:13 -0300

    Add dynamic toggle to add/close form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AddTask from './components/AddTask';
 import Header from './components/Header';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -57,8 +58,11 @@ const App = () => {
   return (
     <div className="container">
       <>
-        <Header />
-        <AddTask onAdd={addTask} />
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ? (
           <Tasks
             tasks={tasks}
