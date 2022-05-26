091b2da610940d408f51ab1243fa412eff3b5260
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-26 18:03:52 -0300

    Add toggle functionality and REMINDER UI

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
 import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task, onDelete }) => (
-  <div className="task">
+const Task = ({ task, onDelete, onToggle }) => (
+  <div
+    className={`task ${task.reminder ? 'reminder' : ''}`}
+    onDoubleClick={() => onToggle(task.id)}
+  >
     <h3>
-      {task.text}
-      {' '}
+      {task.text}{' '}
       <FaTimes
         style={{ color: 'red', cursor: 'pointer' }}
         onClick={() => onDelete(task.id)}
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,10 @@
 // import { useState } from 'react';
 import Task from './Task';
 
-export const Tasks = ({ tasks, onDelete }) => (
+export const Tasks = ({ tasks, onDelete, onToggle }) => (
   <>
     {tasks.map((task) => (
-      <Task key={task.id} task={task} onDelete={onDelete} />
+      <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
     ))}
   </>
 );
