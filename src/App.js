6dde0283a575e1e27a8ba12c6e0aafd5f7c1ce57
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-27 17:54:32 -0300

    Added delete Async

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 // @ts-nocheck
 // // @ts-nocheck
 // import React from "react";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
 import { Tasks } from './components/Tasks';
 import AddTask from './components/AddTask';
 
 import Header from './components/Header';
 
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks');
+  const data = await res.json();
+  return data;
+};
+
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30 pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30 pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Leisure',
-      day: 'Feb 6th at 1:30 pm',
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []);
 
   // Add Task
 
@@ -41,7 +37,9 @@ const App = () => {
 
   // Delete task
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
